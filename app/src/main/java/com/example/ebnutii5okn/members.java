f4a91a0d5f77da0b2d911f5b4928e3b33f3255a6
krgxee <arsenuiseny@gmail.com>
2021-12-10 20:53:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ebnutii5okn/members.java
@@ -0,0 +1,15 @@
+package com.example.ebnutii5okn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class members extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_members);
+    }
+
+}
